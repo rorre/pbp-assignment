b44d3ef09eb2eaa4951c7b75b057a30023ac987f
Ren <renrror@gmail.com>
2022-09-19 15:33:05 +0700

    Add models

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class MyWatchList(models.Model):
+    watched = models.BooleanField()
+    title = models.CharField(max_length=255)
+    rating = models.FloatField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ]
+    )
+    release_date = models.DateField()
+    review = models.TextField()

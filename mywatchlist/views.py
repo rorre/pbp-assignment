8072bea88fa959082b26bd4468c4b9ea09406726
Ren <renrror@gmail.com>
2022-09-20 21:39:34 +0700

    Add view logic

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
 
-# Create your views here.
+from mywatchlist.models import MyWatchList
+
+
+def show_json(request: HttpRequest):
+    data = MyWatchList.objects.all()
+    return HttpResponse(
+        serializers.serialize("json", data),
+        content_type="application/json",
+    )
+
+
+def show_xml(request: HttpRequest):
+    data = MyWatchList.objects.all()
+    return HttpResponse(
+        serializers.serialize("xml", data),
+        content_type="application/xml",
+    )
+
+
+def show_html(request: HttpRequest):
+    data = MyWatchList.objects.all()
+    ctx = {"data": data}
+    return render(request, "list.html", ctx)

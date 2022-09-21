79310b4d1bf94280078de9e83db69b9300b0c843
Ren <renrror@gmail.com>
2022-09-21 12:38:16 +0700

    Add tests for mywatchlist

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from mywatchlist.models import MyWatchList
+
+
+class MyWatchListTest(TestCase):
+
+    SAMPLE = {
+        "watched": True,
+        "title": "Everything Everywhere All At Once",
+        "rating": 4,
+        "release_date": "2022-06-22",
+        "review": "Very epic",
+    }
+
+    def setUp(self) -> None:
+        MyWatchList.objects.create(**self.SAMPLE)
+
+    def test_200(self):
+        response = self.client.get(reverse("mywatchlist:show_json"))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse("mywatchlist:show_xml"))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse("mywatchlist:show_xml"))
+        self.assertEqual(response.status_code, 200)

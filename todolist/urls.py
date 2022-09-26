219b92548a62d4130946411e29128342742d7072
Ren <renrror@gmail.com>
2022-09-26 15:44:28 +0700

    Add auth urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
+from todolist.views import login_user, logout_user, register
+
 
 app_name = "todolist"
 
-urlpatterns = []
+urlpatterns = [
+    path("login/", login_user, name="login_user"),
+    path("register/", register, name="register"),
+    path("logout/", logout_user, name="logout_user"),
+]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,37 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def register(request: HttpRequest):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Akun telah berhasil dibuat!")
+            return redirect("todolist:login")
+
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login_user(request: HttpRequest):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("todolist:show_todos")
+        else:
+            messages.info(request, "Username atau Password salah!")
+    return render(request, "login.html")
+
+
+def logout_user(request: HttpRequest):
+    logout(request)
+    return redirect("todolist:login_user")

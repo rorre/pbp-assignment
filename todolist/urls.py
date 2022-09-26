6b09a3f93a5c6d444e4e91224f7d827f4d808b26
Ren <renrror@gmail.com>
2022-09-26 16:32:16 +0700

    Create/list functionality

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from todolist.views import login_user, logout_user, register
+from todolist.views import login_user, logout_user, register, show_todos, create_todo
 
 
 app_name = "todolist"
@@ -9,4 +9,6 @@ urlpatterns = [
     path("login/", login_user, name="login_user"),
     path("register/", register, name="register"),
     path("logout/", logout_user, name="logout_user"),
+    path("create/", create_todo, name="create_todo"),
+    path("", show_todos, name="show_todos"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,19 @@
+from django import forms
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from todolist.models import Task
+
+
+class NewTodoForm(forms.Form):
+    date = forms.DateField(label="Tanggal")
+    title = forms.CharField(label="Judul")
+    description = forms.CharField(label="Deskripsi", widget=forms.Textarea)
 
 
 def register(request: HttpRequest):
@@ -13,7 +23,7 @@ def register(request: HttpRequest):
         if form.is_valid():
             form.save()
             messages.success(request, "Akun telah berhasil dibuat!")
-            return redirect("todolist:login")
+            return redirect("todolist:login_user")
 
     context = {"form": form}
     return render(request, "register.html", context)
@@ -35,3 +45,29 @@ def login_user(request: HttpRequest):
 def logout_user(request: HttpRequest):
     logout(request)
     return redirect("todolist:login_user")
+
+
+@login_required(login_url="/todolist/login")
+def show_todos(request: HttpRequest):
+    todos = Task.objects.filter(user=request.user).all()
+    ctx = {"todos": todos}
+    return render(request, "list_todos.html", ctx)
+
+
+def create_todo(request: HttpRequest):
+    if request.method == "POST":
+        form = NewTodoForm(request.POST)
+        if form.is_valid():
+            task = Task(
+                date=form.cleaned_data["date"],
+                title=form.cleaned_data["title"],
+                description=form.cleaned_data["description"],
+                user=request.user,
+            )
+            task.save()
+            messages.success(request, "Berhasil disimpan!")
+            return redirect("todolist:show_todos")
+
+    form = NewTodoForm()
+    ctx = {"form": form}
+    return render(request, "create.html", ctx)
